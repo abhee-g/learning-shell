dbf7d55ddc111b161eb40a44f7cf506a2da783d3
Raghu K <raghuk.vit@gmail.com>
2022-11-18 08:42:06 +0530

    MySQL Script

---
+++ 02-vars.sh
@@ -0,0 +1,9 @@
+## Variable - Variables are nothing but, If we assign a name to a set of data is called as variable .
+
+# Declare a varibale
+# VARNAME=DATA (NO Spaces)
+a=10
+
+# Access variable - $VARNAME - Meaning $ symbol prefixing it
+echo A - $a
+
