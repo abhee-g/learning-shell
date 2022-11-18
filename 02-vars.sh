3fa67d248764ad7e568504ce20623525e052b424
Raghu K <raghuk.vit@gmail.com>
2022-11-18 08:46:27 +0530

    MySQL Script

--- 02-vars.sh
+++ 02-vars.sh
@@ -7,3 +7,10 @@ a=10
 # Access variable - $VARNAME - Meaning $ symbol prefixing it
 echo A - $a
 
+# a=10 is hardcoded manually, However we need the data dynaically. This
+## Dynamically you can get in two ways.
+# 1. User Input
+# 2. Script find out with other commands
+
+# Command Substitution
+# VARNAME=$(command)
